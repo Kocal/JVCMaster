150a781f64e1c9602dc08486fd64546a34c72d00
Kocal <hugo@hugo-1001PXD.(none)>
2012-08-23 14:42:16 +0200

    Extension Google Chrome/Chromium + modification des étapes d'installation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-jQuery(function(){
-    $('nav a[href^="#"]').click(function(){  
+jQuery(function($){
+    // Détection du browser
+    var installButton = $('#download_JVCMaster');
+    var userAgent = navigator.userAgent;
+
+    if(userAgent.indexOf('Chromium') !== -1) installButton.attr('name', 'install_GoogleChromeAndChromium');
+    else if(userAgent.indexOf('Chrome') !== -1) installButton.attr('name', 'install_GoogleChromeAndChromium');
+    else if(userAgent.indexOf('Firefox') !== -1) installButton.attr('name', 'install_Firefox');
+    else if(userAgent.indexOf('Opera') !== -1) installButton.attr('name', 'install_Opera');
+    else if(userAgent.indexOf('Safari') !== -1) installButton.attr('name', 'install_Safari');
+
+    $('#download_JVCMaster').click(function(e){
+        var to = $(this).attr('name')
+        $('html, body').animate({
+            scrollTop : $('#' + to).offset().top - 100
+        })
+        e.preventDefault();
+    });
+
+
+    // Navigation animée 
+    $('nav a[href^="#"]').click(function(e){  
         var t = $(this);
         var the_id = t.attr("href");  
 
@@ -8,10 +28,12 @@ jQuery(function(){
 
         $('html, body').animate({  
             scrollTop : $(the_id).offset().top - 60  
-        }, 'slow');  
-        return false;  
-    });  
+        }, 'slow');
+        
+        e.preventDefault();
+    }); 
 
+    // FancyBox
     $("a.fancybox").fancybox({
         helpers : {
             title: {
