9248516fd6302c7c95ce4193dd84e3e79b1fa9d1
Hugo Alliaume <kocal@live.fr>
2012-08-26 00:21:33 +0200

    Bug

--- userscript_v2/JVCMaster.user.js
+++ userscript_v2/JVCMaster.user.js
@@ -678,7 +678,9 @@ var xhr = new XMLHttpRequest();
         if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
             var newVersion = xhr.responseText;
             if(versionCompare(JVCMaster_version, newVersion )){
-                if(localStorage.getItem('JVCMaster_dontUpdateVersion') != newVersion){
+                if(localStorage.getItem('JVCMaster_dontUpdateVersion')
+                    && localStorage.getItem('JVCMaster_dontUpdateVersion') !== ''
+                    && localStorage.getItem('JVCMaster_dontUpdateVersion') != newVersion){
                     function JVCMaster_updateNotification(newVersion){
                     var lb_popup = document.querySelector('#JVCMaster_LightBox_popup');
                     lb_popup.style.padding = "5px 5px 0";
